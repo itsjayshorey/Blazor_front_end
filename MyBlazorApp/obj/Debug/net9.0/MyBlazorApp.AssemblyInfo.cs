fef53edbd8f3ab32be4bb2ddda54206a0054a8eb
itsjayshorey <itsjayshorey@gmail.com>
2025-02-24 17:21:37 +0530

    Your commit message

--- MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
+++ MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e849f7eb2791d0d04c21d8efdb68841785f014c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8a9170d0ade20d434c56044d163bb6f57c842d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
