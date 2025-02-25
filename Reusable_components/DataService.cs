cbb0244b3ce9b6becb6ebc005a1cf21bdc6fcc35
itsjayshorey <itsjayshorey@gmail.com>
2025-02-25 18:45:25 +0530

    Updated CSS styles and HTML layout

--- Reusable_components/DataService.cs
+++ Reusable_components/DataService.cs
@@ -1,11 +1,9 @@
-//This is a dependency injection
 using System.Collections.Generic;
-using System.Threading.Tasks;
+
 public class DataService
 {
-    public async List<string> GetData()
-    {
-        await Task.Delay(500);
-        return new List<string> { "Item 1", "Item 2", "Item 3" };
-    }
+    public List<string> GetData()
+    {
+        return new List<string> { "Item 1", "Item 2", "Item 3" };
+    }
 }
